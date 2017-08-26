78e12ea6ffc0d03f18f07a45c56ce665f8acf9c5
Seiji Matsuoka <mojaie@aol.com>
2017-08-26 12:43:40 +0900

    update docs

--- docs/sw.js
+++ docs/sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["control.html","50fbad1de64fbbb06911dc5856eeac11"],["control.js","a839397cc46596a9116d9f13bb35a77e"],["customMethods.js","89cddff818a2abb3bb161eaf61fd952a"],["datatable.html","349cadd6f235db2d86574528d45e2fb0"],["datatable.js","57d171e3f76a285fd83df992423bab44"],["default.css","bea7f6760af3eb932611c174ebed922a"],["graph.html","3101565191f774cc26dafa6869e98bbc"],["graph.js","03585b66094ccfb04ebb83268c1e225f"],["graphview.js","daa107f1ee9887798ccd84c399bfa493"],["profile.html","c7dab61c4cb6b64ad1dfa29a625d9040"],["profile.js","4fecbd24d486606419a9f4a89fc9b42d"],["report.html","f9059ce0f019dd1a91c41ea0c64343b3"],["report.js","8d16f90b17cf835dbdf7e333edcea522"]];
+var precacheConfig = [["control.html","7bd55d9d9ece0ff3910360de044db142"],["customMethods.js","89cddff818a2abb3bb161eaf61fd952a"],["datatable.html","6d6b8d83c118bc80dba58c8b913f951e"],["default.css","bea7f6760af3eb932611c174ebed922a"],["graph.html","fe1ab77ceaa96cd2001eebfb4165aa43"],["kwcontrol.js","e33e76705d11b51fc4611a390b744c06"],["kwdatatable.js","dc39b1f2373c91cad5cc1e9e01c84308"],["kwgraph.js","68cd6f58b1af12ed3e141e1a3ee13f9d"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
