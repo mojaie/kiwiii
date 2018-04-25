f28d0021bf931176af0e8ab6be05ec27b434ea19
Seiji Matsuoka <mojaie@aol.com>
2018-04-25 23:47:52 +0900

    profile datagrids

--- src/profile.js
+++ src/profile.js
@@ -9,99 +9,101 @@ import {default as fetcher} from './common/fetcher.js';
 import {default as button} from './component/button.js';
 import {default as table} from './component/table.js';
 
+import DatagridState from './datagrid/state.js';
+import {default as view} from './datagrid/view.js';
+import {default as rowf} from './datagrid/rowfilter.js';
 
-function updateChem(compoundID, resources) {
-  d3.select('title').text(compoundID);
-  const query = {
+
+function fetchProfile(compoundID, resources) {
+  const profile = {};
+  // Compound properties
+  const cmpdQuery = {
     workflow: 'search',
     targets: resources.filter(e => e.domain === 'chemical').map(e => e.id),
     key: 'compound_id',
     values: [compoundID]
   };
-  return fetcher.get(query.workflow, query)
+  return fetcher.get(cmpdQuery.workflow, cmpdQuery)
     .then(fetcher.json)
     .then(res => {
-      const rcd = res.records[0];
-      d3.select('#compoundid').html(rcd.compound_id);
-      d3.select('#compounddb').html(
-        resources.find(e => e.id === rcd.__source).name);
-      d3.select('#structure').html(rcd.structure);
-      const fields = misc.defaultFieldProperties([
-        {key: 'key'}, {key: 'value'}
-      ]);
-      const records = res.fields
-        .filter(e => !['structure', 'index', 'compound_id'].includes(e.key))
-        .map(e => ({ key: e.name, value: rcd[e.key] }));
-      d3.select('#properties')
-        .call(table.render, null, null, fields, records);
-      return rcd;
-    }, fetcher.error);
-}
-
-
-function updateChemAliases(resources, qrcd) {
-  const query = {
-    workflow: 'exact',
-    targets: resources.filter(e => e.domain === 'chemical').map(e => e.id),
-    queryMol: {
-      format: 'dbid',
-      source: qrcd.__source,
-      value: qrcd.compound_id
-    },
-    params: {ignoreHs: true}
-  };
-  return fetcher.get(query.workflow, query)
+      profile.compound = res.records[0];
+    })
+    .then(() => {
+      const aliasQuery = {
+        workflow: 'exact',
+        targets: resources.filter(e => e.domain === 'chemical').map(e => e.id),
+        queryMol: {
+          format: 'dbid',
+          source: profile.compound.__source,
+          value: profile.compound.compound_id
+        },
+        params: {ignoreHs: true}
+      };
+      return fetcher.get(aliasQuery.workflow, aliasQuery);
+    })
     .then(fetcher.json)
     .then(res => {
-      const fields = misc.defaultFieldProperties([
-        {key: 'compound_id'}, {key: 'database'}
-      ]);
-      const records = res.records
-        .filter(rcd => rcd.compound_id !== qrcd.compound_id ||
-                rcd.__source !== qrcd.__source)
-        .map(rcd => {
-          return {
-            compound_id: `<a href="profile.html?compound=${rcd.compound_id}" target="_blank">${rcd.compound_id}</a>`,
-            database: resources.find(e => e.id === rcd.__source).name
-          };
-        });
-      d3.select('#aliases')
-        .call(table.render, fields, records);
-    }, fetcher.error);
-}
-
-
-function updateActivities(compoundID, resources) {
-  // Prevent implicit submission
-  /* TODO: instant search component
-  document.getElementById('search')
-    .addEventListener('keypress', event => {
-      if (event.keyCode === 13) event.preventDefault();
+      profile.aliases = res;
+      profile.aliases.fields = misc.defaultFieldProperties(profile.aliases.fields);
+      /*profile.aliases = res.records.filter(rcd => {
+        return rcd.compound_id !== profile.compound.compound_id ||
+        rcd.__source !== profile.compound.__source;
+      });
+      */
+    })
+    .then(() => {
+      const assayQuery = {
+        workflow: 'profile',
+        compound_id: compoundID,
+        targets: resources.filter(e => e.domain === 'activity').map(e => e.id),
+      };
+      return fetcher.get(assayQuery.workflow, assayQuery);
+    })
+    .then(fetcher.json)
+    .then(res => {
+      profile.assays = res;
+      profile.assays.fields = misc.defaultFieldProperties(profile.assays.fields);
+      console.log(JSON.parse(JSON.stringify(profile)));
+      return profile;
     });
-  d3.select('#search').on('keyup', function () {
-    const match = obj => Object.values(obj)
-      .some(e => fmt.partialMatch(d3form.value(this), e));
-    d3.select('#results tbody').selectAll('tr')
-      .style('visibility', d => match(d) ? null : 'hidden')
-      .style('position', d => match(d) ? null : 'absolute');
-  });
-  */
-  const query = {
-    workflow: 'profile',
-    compound_id: compoundID,
-    targets: resources.filter(e => e.domain === 'activity').map(e => e.id),
-  };
-  return fetcher.get(query.workflow, query)
+}
+
+
+function update(compoundID) {
+  fetcher.get('schema')
     .then(fetcher.json)
+    .then(schema => fetchProfile(compoundID, schema.resources))
+    .catch(() => {
+      console.info('Server did not respond');
+      return {
+        aliases: {},
+        assays: {},
+        compound: {}
+      };
+    })
     .then(res => {
-      const fields= misc.defaultFieldProperties([
-        {key: 'assay_id', name: 'Assay ID', format: 'text'},
-        {key: 'value_type', name: 'Value type', format: 'text'},
-        {key: 'value', name: 'Value', format: 'numeric'}
-      ]);
-      // TODO: use datagrid?
-      d3.select('#results').call(table.render, null, null, fields, res.records);
-    }, fetcher.error);
+      d3.select('#cid').text(res.compound.compound_id);
+      d3.select('#db').text(res.compound.__source);
+      d3.select('#struct').html(res.compound.structure);
+
+      const aliasState = new DatagridState(res.aliases);
+      const aliasFilter = d3.select('#aliases').append('div')
+          .classed('alias-filter', true);
+      const aliasdg = d3.select('#aliases').append('div')
+          .classed('alias-dg', true);
+      aliasState.fixedViewportHeight = 200;
+      aliasdg.call(view.datagrid, aliasState);
+      aliasFilter.call(rowf.setFilter, aliasState);
+
+      const assayState = new DatagridState(res.assays);
+      const assayFilter = d3.select('#assays').append('div')
+          .classed('assay-filter', true);
+      const assaydg = d3.select('#assays').append('div')
+          .classed('assay-dg', true);
+      assayState.fixedViewportHeight = 200;
+      assaydg.call(view.datagrid, assayState);
+      assayFilter.call(rowf.setFilter, assayState);
+    });
 }
 
 
@@ -114,68 +116,46 @@ function run() {
       .attr('target', '_blank');
 
   // Compound row
-  const chemDataRow = d3.select('#contents')
-    .append('div')
-      .classed('row', true);
-  const firstCol = chemDataRow.append('div')
-      .classed('col-sm-4', true);
-  firstCol.append('h5').text('Compound ID');
-  firstCol.append('div')
-      .attr('id', 'compoundid')
-      .classed('mb-2');
-  firstCol.append('h5').text('Database');
-  firstCol.append('div')
-      .attr('id', 'compounddb')
-      .classed('mb-2');
-  firstCol.append('h5').text('Structure');
-  firstCol.append('div')
-      .attr('id', 'structure')
-      .classed('mb-2');
-  const propCol = chemDataRow.append('div')
-      .classed('col-sm-4', true);
-  propCol.append('h5').text('Compound properties');
-  propCol.append('table')
-      .attr('id', 'properties')
-      .style('display', 'inline-block');
-  const aliaseCol = chemDataRow.append('div')
-      .classed('col-sm-4', true);
-  aliaseCol.append('h5').text('Compound structure aliases');
-  aliaseCol.append('table')
+  const contents = d3.select('#contents');
+  const cid = contents.append('div');
+  contents.style('counter-reset', 'section');
+  cid.append('h2').text('Compound ID');
+  cid.append('div')
+      .attr('id', 'cid')
+      .classed('mb-2', true);
+  const db = contents.append('div');
+  db.append('h2').text('Database');
+  db.append('div')
+      .attr('id', 'db')
+      .classed('mb-2', true);
+  const struct = contents.append('div');
+  struct.append('h2').text('Structure');
+  struct.append('div')
+      .attr('id', 'struct')
+      .classed('mb-2', true);
+  const props = contents.append('div');
+  props.append('h2').text('Chemical properties');
+  props.append('div')
+      .attr('id', 'props')
+      .classed('mb-2', true);
+  const aliases = contents.append('div');
+  aliases.append('h2').text('Aliases');
+  aliases.append('div')
       .attr('id', 'aliases')
-      .style('display', 'inline-block');
+      .classed('mb-2', true);
+  const assays = contents.append('div');
+  assays.append('h2').text('Assay results');
+  assays.append('div')
+      .attr('id', 'assays')
+      .classed('mb-2', true);
+
   const compoundID = misc.URLQuery().compound || null;
-
-  // Result row
-  const resultRow = d3.select('#contents')
-    .append('div')
-      .classed('row', true);
-  const results = resultRow.append('div')
-      .classed('col-sm-12', true);
-  results.append('h5').text('Assay results and annotations');
-  results.append('input')
-      .classed('form-control', true)
-      .attr('type', 'text')
-      .attr('placeholder', 'Search')
-      .attr('id', 'search');
-  results.append('table')
-      .attr('id', 'results');
-
   if (!compoundID) return;
   // TODO: offline mode flags
   const localFile = document.location.protocol !== "file:";
   const offLine = 'onLine' in navigator && !navigator.onLine;
-  return fetcher.get('schema')
-    .then(fetcher.json)
-    .then(schema => {
-      console.info('Off-line mode is disabled for debugging');
-      return Promise.all([
-        updateChem(compoundID, schema.resources)
-          .then(qrcd => updateChemAliases(schema.resources, qrcd)),
-        updateActivities(compoundID, schema.resources)
-      ]);
-    }, () => {
-      console.info('Server did not respond');
-    });
+  console.info('Off-line mode is disabled for debugging');
+  return update(compoundID);
 }
 
 
