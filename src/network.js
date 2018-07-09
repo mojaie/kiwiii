00c4d5de17c1a35b6e63bcb6ea480f728704b09f
Seiji Matsuoka <mojaie@aol.com>
2018-07-09 12:33:42 +0900

    1.0.0-alpha.1

--- src/network.js
+++ src/network.js
@@ -4,11 +4,11 @@
 
 import NetworkState from './network/state.js';
 import {default as control} from './network/controlBox.js';
-import {default as view} from './network/view.js';
+import {default as component} from './network/component.js';
 import {default as force} from './network/force.js';
 import {default as interaction} from './network/interaction.js';
 
 
 export default {
-  NetworkState, control, view, force, interaction
+  NetworkState, control, component, force, interaction
 };
