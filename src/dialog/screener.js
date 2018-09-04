7d22c7c41fb5c621cd3bc98253e9f4834fae24cd
Seiji Matsuoka <mojaie@aol.com>
2018-09-04 23:25:04 +0900

    screenerDialog WIP

--- src/dialog/screener.js
+++ src/dialog/screener.js
@@ -22,30 +22,67 @@ function menuLink(selection) {
 
 
 function body(selection) {
-  // TODO: QCSession tree
-  // no checkboxes
-  // select only leaf nodes (QCSession)
-  // generate tree from path (/screener/user/assay/...)
   const dialog = selection.call(modal.submitDialog, id, title);
   dialog.select('.modal-dialog').classed('modal-lg', true);
   dialog.select('.modal-body').append('div')
-    .classed('qcsession', true)
-    .call(tree.tree);
+    .classed('qcsession', true);
 }
 
 
-function updateBody(selection, schema, dgfields) {
-
+function updateBody(selection) {
+  // TODO: QCSession tree
+  // no checkboxes
+  // select only leaf nodes (QCSession)
+  const mbody = selection.select('.modal-body');
+  return fetch(`../screener/qcsession`, { credentials: 'include' })
+    .then(fetcher.json)
+    .then(res => {
+      const treeNodes = [];
+      res.records.forEach(rcd => {
+        const dirs = rcd.path.split('/');
+        dirs.unshift();
+        dirs.forEach((dir, i) => {
+          if (!treeNodes.find(e => dir === e.id)) {
+            const n = {id: dir, parent: dirs[i - 1] || 'root'};
+            if (i === dirs.length - 1) {
+              n.concat(rcd);
+            }
+            treeNodes.push(n);
+          }
+        });
+      });
+      mbody.select('.qcsession')
+          .call(tree.tree()
+            .bodyHeight(300)
+            .nodeEnterFactory(qcsNode)
+            .nodeMergeFactory(updateQcsNode), treeNodes
+          );
+    });
 }
 
 
-function execute(selection, compounds, schema) {
+function execute(selection) {
   // TODO: fetch 3 responses and build 3 views
   // generate tile with platevalues
   // generate datagrid with platestats
   // if compounds: generate datagrid with compound
 }
 
+
+function qcsNode(selection, record) {
+  selection.append('span').classed('arrow', true);
+  selection.append('span')
+      .classed('label', true);
+  if (record.QCSRefID) {
+    // TODO: toggle selection state
+  }
+}
+
+
+function updateQcsNode(selection, record) {
+  selection.select('.label')
+      .text(record.id);
+}
 
 export default {
   menuLink, body, updateBody, execute
